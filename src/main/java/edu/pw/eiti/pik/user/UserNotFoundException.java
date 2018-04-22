78e92e6d321923abc6f8844a266b2f89ff1295ca
Paulina Szwed <31202536+Delebrith@users.noreply.github.com>
2018-04-22 15:06:49 +0200

    Revert "Revert 12 feature/pik 42"

--- src/main/java/edu/pw/eiti/pik/user/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package edu.pw.eiti.pik.user;
-
-public class UserNotFoundException extends RuntimeException {
-
-    public UserNotFoundException() {
-        super("No user found for given credentials");
-    }
-}
