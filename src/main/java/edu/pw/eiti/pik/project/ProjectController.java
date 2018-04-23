ad49ae5951bc34efe7f82a0d6eff125ed61b1b29
JakubJakobczyk <kub.jakobczyk@gmail.com>
2018-04-23 14:59:05 +0200

    Added security role check before executing method. Added swagger

--- src/main/java/edu/pw/eiti/pik/project/ProjectController.java
+++ src/main/java/edu/pw/eiti/pik/project/ProjectController.java
@@ -1,7 +1,9 @@
 package edu.pw.eiti.pik.project;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +16,10 @@ public class ProjectController
     @Autowired
     private ProjectService projectService;
 
+    @PreAuthorize("hasRole('EMPLOYER')")
+    @ApiOperation(value = "Add new project", notes = "Adds project into database")
     @PostMapping(path = "/project/add")
+    @ApiParam(value = "Project details", required = true)
     void addProject(@RequestBody ProjectDto projectDto) {
         Project project = projectMapper.fromDto(projectDto);
         projectService.createProject(project);
