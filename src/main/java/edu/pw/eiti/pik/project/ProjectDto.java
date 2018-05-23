01d220a84b795c0e87af8d496db601fd192431f5
llepak <jaluki@vp.pl>
2018-05-23 19:02:12 +0200

    [PIK-48] Changed Project DTO to delete unnecessary isPaid information.

--- src/main/java/edu/pw/eiti/pik/project/ProjectDto.java
+++ src/main/java/edu/pw/eiti/pik/project/ProjectDto.java
@@ -14,7 +14,6 @@ public class ProjectDto {
     private Long id;
     private String name;
     private String description;
-    private Boolean isPaid;
     private Boolean isGraduateWork;
     private Integer numberOfParticipants;
     private ProjectStatus status;
